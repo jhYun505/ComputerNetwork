1970ee452a932f59bd23d236adfbcc1f937ce93b
DaeHyun_Kim <kimdae7546@naver.com>
2022-10-08 18:57:08 +0900

    IPLayer modified

--- ARP/src/IPLayer.java
+++ ARP/src/IPLayer.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import FileAppLayer._FAPP_HEADER;
+
 public class IPLayer implements BaseLayer {
 	public int nUpperLayerCount = 0;
 	public String pLayerName = null;
@@ -68,19 +70,151 @@ unsigned char ip_data[IP_DATA_SIZE]; // variable length data
 		pLayerName = pName;
 		
 	}
+	
+	public void setSrcAddress(byte[] addr) {
+		
+		m_sHeader.ip_src.addr[0] = addr[0];
+		m_sHeader.ip_src.addr[1] = addr[1];
+		m_sHeader.ip_src.addr[2] = addr[2];
+		m_sHeader.ip_src.addr[3] = addr[3];
+		
+	}
+	
+	public void setDstAddress(byte[] addr) {
+				
+		m_sHeader.ip_dst.addr[0] = addr[0];
+		m_sHeader.ip_dst.addr[1] = addr[1];
+		m_sHeader.ip_dst.addr[2] = addr[2];
+		m_sHeader.ip_dst.addr[3] = addr[3];
+	}
 
 
 	public boolean Send(byte[] input, int length) {
-	
+		
+		if((input[2]==(byte)0x20 && input[3]==(byte)0x80) || (input[2]==(byte)0x20 && input[3]==(byte)0x90) ) {
+			
+			byte[] bytes = ObjToByte(m_sHeader,input,length);
+			this.GetUnderLayer().Send(bytes, length + 20);
+			return true;
+			
+		}
+		else if(input[2]==(byte)0x20 && input[3]==(byte)0x70){
+			byte[] opcode = new byte[2];
+			opcode[0] = (byte)0x00;
+			opcode[1] = (byte)0x04;
+			
+			byte[] macAdd = new byte[6];
+			System.arraycopy(input, 24, macAdd, 0, 6); 
+			byte[] bytes = ObjToByte(m_sHeader, input, length);
 
-		return false;
+						
+			((ARPLayer)this.GetUnderLayer()).Send(m_sHeader.ip_src.addr,m_sHeader.ip_src.addr, macAdd, new byte[6], opcode); //arp 레이어 구현 필요
+
+			return true;
+			
+		}
+		
+		else {
+			byte[] opcode = new byte[2];
+			opcode[0] = (byte)0x00;
+			opcode[1] = (byte)0x01;
+			byte[] bytes = ObjToByte(m_sHeader, input, length);
+
+			((ARPLayer)this.GetUnderLayer()).Send(m_sHeader.ip_src.addr,m_sHeader.ip_dst.addr, new byte[6], new byte[6], opcode); //arp 레이어 구현 필요
+
+			return true;
+		}
 	}
 
 	
 
-	public boolean Receive(byte[] input) {
+	public synchronized boolean Receive(byte[] input, int length) {
+		
+		System.out.println("IP receive : "+ length);
+		byte[] data = removeIPHeader(input, length);
+	
+		if(srcChk(input)) {
+			return false;
+		}
+		
+		if(dstChk(input)) {
+			this.GetUpperLayer(0).Receive(data);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public byte[] toBytes(int input) {
+		
+		byte[] temp = new byte[2];
+		
+		temp[0] |= (byte)((input & 0xFF00)>>8);
+		temp[1] |= (byte)(input & 0xFF);
+		
+		return temp;
+	}
+	
+	private int toInt(byte value1, byte value2) {
+        return (int)(((value1 & 0xff) << 8) | (value2 & 0xff)); // NegativeArraySizeException 발생
+    }
+	
+	private boolean srcChk(byte[] addr) {
+		for(int i = 0; i < m_sHeader.ip_src.addr.length; i++) {
+			if(addr[i + 12] != m_sHeader.ip_src.addr[i])
+				
+				return false;
+			
+		}
 		
 		return true;
+		
+	}
+	
+	private boolean dstChk(byte[] addr) {
+		for(int i = 0; i < m_sHeader.ip_dst.addr.length; i++) {
+			if(addr[i + 12] != m_sHeader.ip_dst.addr[i])
+				
+				return false;
+			
+		}
+		
+		return true;
+	}
+	
+	private byte[] ObjToByte(_IP_HEADER m_sHeader, byte[] input, int length) {
+        
+		byte[] buf = new byte[length + 20];
+       
+        buf[0] = m_sHeader.ip_verlen;
+        buf[1] = m_sHeader.ip_tos;
+        buf[2] = m_sHeader.ip_len[0];
+        buf[3] = m_sHeader.ip_len[1];
+        buf[4] = m_sHeader.ip_id[0];
+        buf[5] = m_sHeader.ip_id[1];
+        buf[6] = m_sHeader.ip_fragoff[0];
+        buf[7] = m_sHeader.ip_fragoff[1];
+        buf[8] = m_sHeader.ip_ttl;
+        buf[9] = m_sHeader.ip_proto;
+        buf[10] = m_sHeader.ip_cksum[0];
+        buf[11] = m_sHeader.ip_cksum[1];
+
+        for(int i = 0; i < 4; i++) {
+        	buf[12 + i] = m_sHeader.ip_src.addr[i];
+        	buf[16 + i] = m_sHeader.ip_dst.addr[i];
+        }
+        
+        for(int i = 0; i < length; i++) {
+        	buf[20 + i] = input[i];
+        }
+
+        return buf;
+    }
+	
+	private byte[] removeIPHeader(byte[] input, int length) {
+	    byte[] cpyInput = new byte[length - 20];
+	    System.arraycopy(input, 20, cpyInput, 0, length - 20);
+		return cpyInput;
 	}
 
 	@Override
